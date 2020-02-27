e05d45f117628b5a598b93a4a6b4c353f063dfe7
unknown <KalebJohnson45@yahoo.com>
2020-02-27 09:09:55 -0600

    added reset

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -7,20 +7,21 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [totalHome, setTotalHome] = useState(0);
   const [totalAway, setTotalAway] = useState(0);
+  const [totalClock, setTotalClock] = useState(0);
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">Me</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
             <div className="home__score">{totalHome}</div>
           </div>
-          <div className="timer">00:03</div>
+
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">React</h2>
             <div className="away__score">{totalAway}</div>
           </div>
         </div>
@@ -59,10 +60,20 @@ function App() {
                     >
                     Away Field Goal
                     </button>
+                    <button
+                    onClick={() => {
+                      setTotalAway(totalAway - totalAway);
+                      setTotalHome(totalHome - totalHome);
+                    }}
+                    >
+                    RESET
+                    </button>
         </div>
       </section>
     </div>
   );
 }
+
+
 
 export default App;
